57438f5b973e99bc280b9fbc194e3aec4e54bce0
mazen <gshshmazen@gmail.com>
2025-09-26 19:56:57 +0300

    ubdate files

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Nav from './navbar/nav'
-
-
-
+import Nav from './components/navbar/nav'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Nav />
-
-  </StrictMode>,
+  </StrictMode>
+  
 )
