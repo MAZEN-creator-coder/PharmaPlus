d70ed467dea239bf904170c358433d11eca0acaa
NoohaShehab <nohashehab576@gmail.com>
2025-11-14 13:57:24 +0400

    Fixes

--- src/components/navbar/Brand.jsx
+++ src/components/navbar/Brand.jsx
@@ -1,10 +1,17 @@
 import logo from '../../assets/img/logo.png';
 import styles from './Navbar.module.css';
+import {motion} from 'framer-motion';
 
 export default function Brand() {
   return (
     <a className={styles.brand} href="/" aria-label="Go to homepage">
-      <img src={logo} alt="Furniture store logo"  className={styles.logo} />
+      <motion.img 
+        initial={{ y: -70 }} 
+        animate={{ y: 0 }} 
+        transition={{ type: 'spring', stiffness: 120, delay: 0.1 }}
+        src={logo} 
+        alt="PharmaPlus store logo"  
+        className={styles.logo} />
     </a>
   );
 }
